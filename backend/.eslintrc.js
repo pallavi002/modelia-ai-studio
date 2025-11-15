911f199c9485de7dd96021521de5421e82855ce3
Pallavi Lodhi <pallavilodhi@Pallavis-MacBook-Air.local>
2025-11-16 01:01:18 +0530

    linting added

---
+++ backend/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    plugins: ["@typescript-eslint", "prettier"],
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:prettier/recommended",
+    ],
+    rules: {
+      "prettier/prettier": "error",
+      "@typescript-eslint/no-unused-vars": ["warn"],
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  };
+  
